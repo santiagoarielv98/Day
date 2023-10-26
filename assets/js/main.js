e382bef4702604ab2a5a27f1441b906ca9e51cf8
Santiago Ariel <santiagoarielv98@gmail.com>
2023-10-26 19:09:26 -0300

    cambios

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,3 @@
-/**
- * Template Name: Day
- * Updated: Sep 18 2023 with Bootstrap v5.3.2
- * Template URL: https://bootstrapmade.com/day-multipurpose-html-template-for-free/
- * Author: BootstrapMade.com
- * License: https://bootstrapmade.com/license/
- */
 (function () {
   ("use strict");
 
@@ -184,40 +177,6 @@
   }
 
   /**
-   * Porfolio isotope and filter
-   */
-  window.addEventListener("load", () => {
-    let portfolioContainer = select(".portfolio-container");
-    if (portfolioContainer) {
-      let portfolioIsotope = new Isotope(portfolioContainer, {
-        itemSelector: ".portfolio-item",
-      });
-
-      let portfolioFilters = select("#portfolio-flters li", true);
-
-      on(
-        "click",
-        "#portfolio-flters li",
-        function (e) {
-          e.preventDefault();
-          portfolioFilters.forEach(function (el) {
-            el.classList.remove("filter-active");
-          });
-          this.classList.add("filter-active");
-
-          portfolioIsotope.arrange({
-            filter: this.getAttribute("data-filter"),
-          });
-          portfolioIsotope.on("arrangeComplete", function () {
-            AOS.refresh();
-          });
-        },
-        true
-      );
-    }
-  });
-
-  /**
    * Initiate portfolio lightbox
    */
   const portfolioLightbox = GLightbox({
@@ -253,12 +212,4 @@
   /**
    * Animation on scroll
    */
-  window.addEventListener("load", () => {
-    AOS.init({
-      duration: 1000,
-      easing: "ease-in-out",
-      once: true,
-      mirror: false,
-    });
-  });
 })();
